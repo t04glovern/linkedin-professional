19a34a7118098c6eabdf3accd4b6308cb2e09bce
Nathan Glover <nathan@glovers.id.au>
2021-05-19 22:55:48 +0800

    updated it i guess

--- handler.py
+++ handler.py
@@ -1,23 +1,36 @@
 
 from bs4 import BeautifulSoup
 import requests
+import json
+import random
+
+from adjectives import random_adjective
 
 radar_base_url = 'https://www.thoughtworks.com/radar/'
 quadrants = ['techniques', 'tools', 'platforms', 'languages-and-frameworks']
+skill_items = {}
 
 def skills(event, context):
-    skill_items = {}
-    for quadrant in quadrants:
-        target_radar_url = radar_base_url + quadrant
-        target_radar_page = requests.get(target_radar_url)
-        target_radar_soup = BeautifulSoup(target_radar_page.content, 'html.parser')
-        target_radar = target_radar_soup.find(id='responsive-tech-radar')
-        skill_items[quadrant] = skills_from_html(target_radar)
+    # Only update the list if empty
+    if not skill_items:
+        for quadrant in quadrants:
+            target_radar_url = radar_base_url + quadrant
+            target_radar_page = requests.get(target_radar_url)
+            target_radar_soup = BeautifulSoup(target_radar_page.content, 'html.parser')
+            target_radar = target_radar_soup.find(id='responsive-tech-radar')
+            skill_items[quadrant] = skills_from_html(target_radar)
+    
+    body = {
+        'phrase': get_phrase(skill_items)
+    }
 
     return {
         "statusCode": 200,
-        "body": skill_items
+        "body": json.dumps(body)
     }
+
+def get_phrase(skill_items):
+    return 'I think that ' + random_skill(skill_items) + ' are ' + random_adjective() + ' and ' + random_skill(skill_items) + ' should strive to be more ' + random_adjective() + ' because'
 
 def skills_from_html(html_target_radar):
     skill_group = {}
@@ -27,3 +40,13 @@ def skills_from_html(html_target_radar):
         items = radar_option.find_all('li')
         skill_group[option] = list(map(lambda x: x.find('a').text, items))
     return skill_group
+
+def random_skill(skill_items):
+    quadrants = ['techniques', 'tools', 'platforms', 'languages-and-frameworks']
+    options = ['adopt', 'trial', 'assess', 'hold']
+    quadrant = random.choice(quadrants)
+    option = random.choice(options)
+    choice = None
+    while choice is None:
+        choice = random.choice(skill_items[quadrant][option])
+    return choice
